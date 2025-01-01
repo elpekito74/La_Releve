42fd8be69a4c74b287c11d147c9d5d3aaa80ad8a
elpekito74 <83301493+elpekito74@users.noreply.github.com>
2025-01-01 18:04:58 +0100

    supression

--- index2.js
+++
@@ -1,6 +0,0 @@
-const { prefix, token } = require('/config.json');
-
-const { Client, Intents, Collection } = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILD_MEMBERS", "GUILD_MEMBER_ADD"]
-});
-bot.login(token);
